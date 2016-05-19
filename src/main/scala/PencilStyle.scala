592f6c29932468a353219161dd1ef860cb54fe1f
Ldpe2G <liangdepeng@gmail.com>
2016-05-19 17:47:19 +0800

    Update PencilStyle.scala

--- src/main/scala/PencilStyle.scala
+++ src/main/scala/PencilStyle.scala
@@ -3,6 +3,9 @@ import Utils._
 import java.io.InputStreamReader
 import java.io.BufferedReader
 
+/**
+ * @author Depeng Liang
+ */
 object PencilStyle {
   
   def main(args: Array[String]): Unit = {
